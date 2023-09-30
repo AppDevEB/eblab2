07218e31f2e65fad192a83cdb862bc7c875430d3
EdwardBustamante <dwardo258@gmail.com>
2023-09-30 11:05:39 +0800

    main

---
+++ app/Config/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/mymusic', 'MusicController::index');
+$routes->post('/upload', 'MusicController::upload');
+$routes->post('/create', 'MusicController::create');
+$routes->get('/playlists/(:any)', 'MusicController::playlists/$1');
+$routes->get('/search', 'MusicController::search');
+$routes->post('/upload', 'MusicController::upload');
+$routes->post('/add', 'MusicController::add');
+
